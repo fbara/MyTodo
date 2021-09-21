4968b7d8baf0a8ab2d511f2f448566059eccbdb1
fbara <fbara1@yahoo.com>
2021-09-21 10:06:14 -0500

    Initial Commit

---
+++ MyTodo/MyTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodoApp.swift
+//  MyTodo
+//
+//  Created by Frank Bara on 9/21/21.
+//
+
+import SwiftUI
+
+@main
+struct MyTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
